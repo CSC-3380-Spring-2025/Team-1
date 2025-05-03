f01cc626788d7c7ceb306a5a669bb329c973b07f
Gracielle Perez <gracielleprz@gmail.com>
2025-05-03 10:27:51 -0500

    Fixed the movement on the chemistry puzzle
    The screen will not move when the player is on the dropdown UI screen

--- Escape Room B/Assets/ChemistryPuzzle.cs
+++ Escape Room B/Assets/ChemistryPuzzle.cs
@@ -9,21 +9,16 @@ public class ChemistryPuzzle : MonoBehaviour
     public TMP_Text resultText;
     public GameObject chemistryTableUI;
     public GameObject stickyNote;
-    public PlayerController playerController;
 
-    private List<string> correctCombination = new List<string> { "H2O", "NaCl" }; // Player has the ability to change options. Correct answer is H2O and NaCl
+    private List<string> correctCombination = new List<string> { "H2O", "NaCl" }; // You can change this
 
     public void OpenUI()
     {
         chemistryTableUI.SetActive(true);
 
-        // Unlock cursor
+        //unlock cursor
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
-
-        // Stops player movement
-        if (playerController != null)
-        playerController.enabled = false;
     }
 
     public void CloseUI()
@@ -33,10 +28,6 @@ public class ChemistryPuzzle : MonoBehaviour
         // Lock and hide the cursor again
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-
-        // Player can move again
-        if (playerController != null)
-        playerController.enabled = true;
     }
 
     public void MixChemicals()
@@ -44,17 +35,21 @@ public class ChemistryPuzzle : MonoBehaviour
         string chem1 = chemical1Dropdown.options[chemical1Dropdown.value].text;
         string chem2 = chemical2Dropdown.options[chemical2Dropdown.value].text;
 
-        // Put selected chemicals in a list
+        //put selected chemicals in a list
         List<string> selected = new List<string> { chem1, chem2 };
         selected.Sort();
         correctCombination.Sort();
 
-        // Check if the selected chemicals are correct
+        //check if selected chemicals are correct
         if (selected[0] == correctCombination[0] && selected[1] == correctCombination[1]) 
         {
             resultText.text = "Success! A new clue appears!";
             stickyNote.SetActive(true);
-            CloseUI();
+            chemistryTableUI.SetActive(false);
+
+            // lock the mouse again
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
         }
         else
         {

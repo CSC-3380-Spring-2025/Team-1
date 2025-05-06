82908598c646d845850289034b689f21aff61f8d
dogbank101 <criden2@lsu.edu>
2025-05-06 18:12:57 -0500

    added more comments

--- Escape Room B/Assets/Scripts/BallButton.cs
+++ Escape Room B/Assets/Scripts/BallButton.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 
+// this makes sure that the gameobject has a renderer and a collider
 [RequireComponent(typeof(Renderer), typeof(Collider))]
 public class BallButton : MonoBehaviour
 {
-    [Tooltip("Index of this ball (0–11) in the grid, assign in Inspector)")]
+    
     public int index;
 
-    [Tooltip("Color when not yet activated")]
+    
     public Color inactiveColor = Color.red;
-    [Tooltip("Color when correctly activated")]
+    
     public Color activeColor   = Color.green;
 
     Renderer _renderer;
@@ -25,7 +26,6 @@ public class BallButton : MonoBehaviour
         BallGridManager.Instance.HandleBallClick(index, this);
     }
 
-    /// <summary>Called by the manager when this ball is one of the correct eight.</summary>
     public void Activate()
     {
         _renderer.material.color = activeColor;
